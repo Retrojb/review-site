ec72b1db6270c7a71e7aaeb68e3066e39f96dc53
Retrojb <baltes.26@osu.edu>
2018-06-15 10:55:06 -0400

    review controller built

--- src/main/java/org/wecancodeit/reviewsite/ReviewSiteController.java
+++ src/main/java/org/wecancodeit/reviewsite/ReviewSiteController.java
@@ -1,15 +1,26 @@
 package org.wecancodeit.reviewsite;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ReviewSiteController {
 
-	@RequestMapping("/")
+	@Autowired
+	ReviewSiteRepository reviewSiteRepo;
+	
+	@RequestMapping("/ReviewHome")
 	public String reviewHome(Model model) {
-		model.addAttribute("review", null);
-		return "home";
+		model.addAttribute("reviews", reviewSiteRepo.findAll());
+		return "ReviewHome";
+	}
+	
+	@RequestMapping("/review/{id}")
+	public String getReview(@PathVariable(name="id") Long id, Model model) {
+		model.addAttribute("review", reviewSiteRepo.findById(id));
+		return "review";
 	}
 }
